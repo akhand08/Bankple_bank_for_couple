e27fc0d921e57c2dc0e74dbbe9a95a64490baa28
akhand08 <adib08@outlook.com>
2025-03-03 11:15:48 +0600

    Basic api server developed

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akhand08/Bankple_bank_for_couple
 
 go 1.24.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 
+	server := NewAPIServer(":3000")
+	server.Run()
+
 	fmt.Println("Welcome to Bankple")
 
 }
